172e57072f99fb839e8f052693d1ce95427a25bf
ArtunKARA <artunankara@gmail.com>
2022-01-09 20:48:23 +0300

    update

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -10,6 +10,37 @@ namespace Loops
     {
         static void Main(string[] args)
         {
+            for (int i = 2; i <= 100; i+=2)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Finished! For");
+            int number =0;
+            while (number<100)
+            {
+                Console.WriteLine(number);
+                number+=4;
+            }
+            Console.WriteLine("Finished! While");
+            number = 0;
+            do
+            {
+                number--;
+                Console.WriteLine(number);
+            } while (number>0);
+            Console.WriteLine("Finished! Do While");
+            string[] okcular = { "abraham", "gıral corc", "naycıl" };
+            foreach (var item in okcular)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("adam okçular Finished! Foreach");
+
+
+
+
+
+            Console.ReadKey();
         }
     }
 }
